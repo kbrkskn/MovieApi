f514ab9e8ea8336016d07638a6c9ca0f1a668f45
Kübra Kaygusuz <kbrkskn15@gmail.com>
2025-07-10 16:33:46 +0300

    UserRegister

--- Frontends/MovieApi.WebUI/Controllers/MovieController.cs
+++ Frontends/MovieApi.WebUI/Controllers/MovieController.cs
@@ -1,14 +1,40 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MovieApi.Dto.Dtos.MovieDtos;
+using Newtonsoft.Json;
+using NuGet.Versioning;
 
 namespace MovieApi.WebUI.Controllers
 {
     public class MovieController : Controller
     {
-        public IActionResult MovieList()
+        private readonly IHttpClientFactory _httoClientFactory;
+
+        public MovieController(IHttpClientFactory httoClientFactory)
+        {
+            _httoClientFactory = httoClientFactory;
+        }
+
+        public async Task< IActionResult> MovieList()
         {
             ViewBag.v1 = "Film Listesi";
             ViewBag.v2 = "Anasayfa";
             ViewBag.v3 = "Tüm Filmler";
+
+            var client = _httoClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7291/api/Movies");
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultMovieDto>>(jsonData);
+
+                return View(values);
+            }
+
+            return View();
+        }
+        public async Task<IActionResult> MovieDetail(int id)
+        {
+            id = 0;
             return View();
         }
     }
